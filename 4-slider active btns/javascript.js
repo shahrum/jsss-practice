292324c0a36efe88e16552917f49d4c780604583
shahrum <shahrum.hosseinabadi@gmail.com>
2013-02-27 12:48:27 +0330

    ba tarife moteghayere current slide ghabl az n aval kelase li ghabli ra khali karde sepas be n ajdid kelas active midahim,be insurat dar har lahze faghat yek li active ast

--- 4-slider active btns/javascript.js
+++ 4-slider active btns/javascript.js
@@ -1,10 +1,13 @@
 window.onload = function(){
 	var slideshow = document.getElementsByClassName('slideshow').item(0);
-	var train = slideshow.document.getElementsByClassName('train').item(0);
+	var train = slideshow.getElementsByClassName('train').item(0);
 
-	function go2slide(n){
+	var currentslide = 0;
+	go2slide = function(n){
 		train.style.left=(-450*n)+"px";
+		list.item(currentslide).className="";
 		list.item(n).className = "active";
+		currentslide = n;
 	}
 
 	var list = document.getElementsByClassName('btns').item(0).getElementsByTagName('li');
