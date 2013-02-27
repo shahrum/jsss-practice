90d03775f3456a63d3c181d34474b8d679eea0b3
shahrum <shahrum.hosseinabadi@gmail.com>
2013-02-27 23:31:05 +0330

    ta inja slide show ro gerefte va dar marhale badi tamamie li ha ra gerefte im

---
+++ slider start/javascript.js
@@ -0,0 +1,5 @@
+window.onload = function(){
+	var slideshow = document.getElementsByClassName('slideshow').item(0);
+	var list = document.getElementsByClassName('btns').item(0).getElementsByTagName('li');
+
+}
