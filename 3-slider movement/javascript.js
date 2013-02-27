f0d117e9d5af7d60c274fc2245885f10d51430d7
shahrum <shahrum.hosseinabadi@gmail.com>
2013-02-27 12:26:50 +0330

    harkat

---
+++ 3-slider movement/javascript.js
@@ -0,0 +1,18 @@
+window.onload = function(){
+	var slideshow = document.getElementsByClassName('slideshow').item(0);
+	var train = slideshow.document.getElementsByClassName('train').item(0);
+
+	function go2slide(n){
+		train.style.left=(-450*n)+"px";
+	}
+
+	var list = document.getElementsByClassName('btns').item(0).getElementsByTagName('li');
+	for (var i = 0; i < list.length; i++) {
+		(function(j){
+				list.item(j).onclick = function(){
+				go2slide(j);
+			}
+		})(i);
+	};
+
+}
