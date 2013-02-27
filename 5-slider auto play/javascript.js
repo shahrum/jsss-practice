2794a572ba0e05d5f073610decc3e1d1c7a59016
shahrum <shahrum.hosseinabadi@gmail.com>
2013-02-27 12:53:59 +0330

    auto play

---
+++ 5-slider auto play/javascript.js
@@ -0,0 +1,28 @@
+window.onload = function(){
+	var slideshow = document.getElementsByClassName('slideshow').item(0);
+	var train = slideshow.getElementsByClassName('train').item(0);
+
+	var currentslide = 0;
+	go2slide = function(n){
+		train.style.left=(-450*n)+"px";
+		list.item(currentslide).className="";
+		list.item(n).className = "active";
+		currentslide = n;
+	}
+
+	nextslide = function(){
+		go2slide(currentslide + 1);
+	}
+
+	var list = document.getElementsByClassName('btns').item(0).getElementsByTagName('li');
+	for (var i = 0; i < list.length; i++) {
+		(function(j){
+				list.item(j).onclick = function(){
+				go2slide(j);
+			}
+		})(i);
+	};
+
+	setInterval(nextslide,2000);
+
+}
